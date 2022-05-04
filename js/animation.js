5b86bd6c95cf722c0863ee0c5d5ac32ba31cdbd6
pMattheew <patrickmatias.silva@hotmail.com>
2022-05-04 11:59:40 -0300

    bases para animação no banner

--- js/animation.js
+++ js/animation.js
@@ -3,14 +3,28 @@
 let wrapper = document.querySelector('.wrapper');
 
 let tl = gsap.timeline({
-    onComplete: function(){
+    delay: 1,
+    /* onComplete: function(){
         wrapper.classList.add('active');
-    }
+    } z index diminuir ao terminar */
+
+    /* document.getElementById("myH1").setAttribute <- adicionar atributo autoplay ao video ao terminar */
 });
 
+tl.from('.wrapper span',
+{
+    opacity: 0,
+    scale: 1,
+    fontSize: '0.9em',
+
+    duration: 1,
+    stagger: 1
+})
 tl.to('.wrapper',
 {
-    
+    delay: 1,
     duration: 1,
     opacity: 0
 })
+
+
